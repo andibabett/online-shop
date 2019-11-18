2324ec0f30a45c6c7c0c6b8cb854a269d02b1921
andibabett <andi_babett@yahoo.com>
2019-11-18 20:48:46 +0200

    first commit

---
+++ online-shop/src/main/java/org/fasttrackit/onlineshop/persistance/ProductRepository.java
@@ -0,0 +1,6 @@
+package src.main.java.org.fasttrackit.onlineshop.persistance;
+
+import src.main.java.org.fasttrackit.onlineshop.domain.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
